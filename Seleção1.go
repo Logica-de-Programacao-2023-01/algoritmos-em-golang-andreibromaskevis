2ddec2af2917b91c7cb61dae08f3234a3cd9085d
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-14 21:21:06 -0300

    Seleção1.go

---
+++ Seleção1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	fmt.Println("Digite dois números :")
+	fmt.Scanln(&num1, &num2)
+	if num1 > num2 {
+		fmt.Printf("%d é maior que %d", num1, num2)
+	} else if num1 < num2 {
+		fmt.Printf("%d é menor que %d", num1, num2)
+	} else {
+		fmt.Printf("%d é igual a %d", num1, num2)
+	}
+}
