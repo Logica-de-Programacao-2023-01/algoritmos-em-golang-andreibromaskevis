0d6a0c50cf224c35b307bc9bb7b228b85f3b0749
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-21 18:22:48 -0300

    Repetição10.go

---
+++ Repetição10.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero int
+  var maior int
+
+	fmt.Println("Digite varios números (digite 0 para parar):")
+	fmt.Print("Número: ")
+	fmt.Scanln(&numero)
+	maior = numero
+	
+	for numero != 0 {
+		if numero > maior {
+			maior = numero
+		}
+		fmt.Print("Número: ")
+		fmt.Scanln(&numero)
+	}
+	fmt.Println("O maior número é:", maior)
+}
